d6071514ba40e1fa3bb1e69eabf8e7ac11e0801d
ichinose-hezx <ichinosezx@163.com>
2024-03-14 19:47:05 +0800

    add tailrecur and non_tailrecur version of listsum with quickCheck, examing on speed and stacksize

--- listsum.hs
+++ listsum.hs
@@ -1,5 +1,21 @@
-listsum :: [Int] -> Int
-listsum [] = 0
-listsum (x:xs) = sum(xs) + x
+import Test.QuickCheck
 
-main = print( listsum [1,2,3] )
+listsum_non_tail :: [Int] -> Int
+listsum_non_tail [] = 0
+listsum_non_tail (x:xs) = listsum_non_tail (xs) + x
+
+listsum_tail :: [Int] -> Int -> Int
+listsum_tail [] y = y
+listsum_tail (x:xs) y = listsum_tail xs (x+y)
+
+prop_listsum_non_tail :: [Int] -> Bool
+prop_listsum_non_tail xs = listsum_non_tail xs == sum xs
+
+prop_listsum_tail :: [Int] -> Bool
+prop_listsum_tail xs = listsum_tail xs 0 == sum xs
+ 
+main ::IO()
+main = do
+    quickCheck prop_listsum_non_tail
+    quickCheck prop_listsum_tail
+    
